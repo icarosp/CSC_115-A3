4778268486083bf5b5acb854d238f1bbdead0828
icarosp <icaro.ifg@gmail.com>
2014-10-21 14:41:45 -0700

    First commit

---
+++ LLStack.java
@@ -0,0 +1,26 @@
+public class LLStack implements Stack{
+	
+	int size(){
+		return -1;
+	}
+
+	boolean empty(){
+		return false;
+	}
+
+	void push(){
+
+	}
+
+	pop() throws StackEmptyException{
+		return StackEmptyException;
+	} 
+
+	peek() throws StackEmptyException{
+		return StackEmptyException;
+	}
+
+	void makeEmpty(){
+		
+	}
+}
